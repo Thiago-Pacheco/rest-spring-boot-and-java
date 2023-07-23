5c02fc30a28ae774e04a1d98599656e90a3b1bee
Thiago Pacheco <pachecotkd@gmail.com>
2023-07-23 17:57:31 -0300

    First step in java

---
+++ 05_FirstStep/rest-spring-boot-and-java/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,23 @@
+package br.com.erudio;
+
+public class Greeting {
+
+	private final long id;
+
+	private final String contente;
+
+	public Greeting(long id, String contente) {
+		this.id = id;
+		this.contente = contente;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContente() {
+		return contente;
+	}
+
+	
+}
